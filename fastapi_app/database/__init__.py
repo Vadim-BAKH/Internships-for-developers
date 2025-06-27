79554e80145bd23d9000f32d06e82ea6e8bd3778
PC <vadima64@mail.ru>
2025-06-27 19:58:06 +0300

    add import engine

--- fastapi_app/database/__init__.py
+++ fastapi_app/database/__init__.py
@@ -2,11 +2,13 @@
 
 from fastapi_app.database.async_generator import get_session_db
 from fastapi_app.database.db_session import async_engine, async_session
-from fastapi_app.database.test_db_session import test_async_session
+from fastapi_app.database.db_session_test import (async_test_engine,
+                                                  async_test_session)
 
 __all__ = [
     "get_session_db",
     "async_engine",
     "async_session",
-    "test_async_session",
+    "async_test_session",
+    "async_test_engine",
 ]
