185fac4e46a5420444d985e8ef55414af9920cc5
PC <vadima64@mail.ru>
2025-06-27 19:59:01 +0300

    delete import test_url

--- fastapi_app/configs/__init__.py
+++ fastapi_app/configs/__init__.py
@@ -7,10 +7,8 @@
 
 from fastapi_app.configs.loguru_conf import logger
 from fastapi_app.configs.main_conf import settings
-from fastapi_app.configs.test_url import TEST_DB_URL
 
 __all__ = [
     "logger",
     "settings",
-    "TEST_DB_URL",
 ]
