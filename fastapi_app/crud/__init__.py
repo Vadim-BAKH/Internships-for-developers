3ff41b2b4f2334c60b91f49a9079698b8a15058b
PC <vadima64@mail.ru>
2025-06-27 07:50:52 +0300

    for CRUD create user: check email; username

---
+++ fastapi_app/crud/__init__.py
@@ -0,0 +1,7 @@
+"""Инициализация запросов CRUD."""
+
+from fastapi_app.crud.user_crud.create_user import create_user
+
+__all__ = [
+    "create_user",
+]
---
+++ fastapi_app/crud/user_crud/check_username.py
@@ -0,0 +1,28 @@
+"""Проверка уникальности username."""
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from fastapi_app.configs import logger
+from fastapi_app.exceptions import UsernameExists
+from fastapi_app.models import User
+
+
+async def check_name(
+    session: AsyncSession,
+    username: str,
+) -> bool:
+    """
+    Проверяет уникальность username.
+
+    Поднимает исключение, если уже существует имя.
+    """
+    existing_user = await session.scalar(
+        select(User).where(User.username == username)
+    )
+    if existing_user:
+        logger.info("User with username '{}' already exists.", username)
+        raise UsernameExists(username)
+    logger.debug("Username '{}' is available", username)
+
+    return True
