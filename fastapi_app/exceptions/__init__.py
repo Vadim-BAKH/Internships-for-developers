351076b915658017f075decc8f98fb175294c74a
PC <vadima64@mail.ru>
2025-07-02 20:02:16 +0300

    add general errors

--- fastapi_app/exceptions/__init__.py
+++ fastapi_app/exceptions/__init__.py
@@ -1,8 +1,10 @@
 """Инициализация исключений."""
 
 from fastapi_app.exceptions.bad_request import EmailExists, UsernameExists
+from fastapi_app.exceptions.general_errors import register_exception_handler
 
 __all__ = [
     "UsernameExists",
     "EmailExists",
+    "register_exception_handler",
 ]
