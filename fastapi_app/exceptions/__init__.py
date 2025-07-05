f370b636fe277b42c78aeb658ecc4bdab8b5e0c6
PC <vadima64@mail.ru>
2025-07-05 15:52:12 +0300

    import linter

--- fastapi_app/exceptions/__init__.py
+++ fastapi_app/exceptions/__init__.py
@@ -1,10 +1,20 @@
 """Инициализация исключений."""
 
-from fastapi_app.exceptions.bad_request import EmailExists, UsernameExists
+from fastapi_app.exceptions.bad_request import (
+    EmailExists,
+    PasswordsDoNotMatch,
+    UsernameExists,
+)
+from fastapi_app.exceptions.forbidden import UserInActive
 from fastapi_app.exceptions.general_errors import register_exception_handler
+from fastapi_app.exceptions.unauthorizes import InvalidToken, UserUnauthorized
 
 __all__ = [
     "UsernameExists",
     "EmailExists",
+    "UserUnauthorized",
     "register_exception_handler",
+    "PasswordsDoNotMatch",
+    "UserInActive",
+    "InvalidToken",
 ]
--- fastapi_app/models/__init__.py
+++ fastapi_app/models/__init__.py
@@ -2,11 +2,13 @@
 
 from fastapi_app.models.base import Base, BaseModel
 from fastapi_app.models.mixins.pk_mix import IntIdPkMixin
+from fastapi_app.models.mixins.soft_delete import ActiveMixin
 from fastapi_app.models.user import User
 
 __all__ = [
     "Base",
     "BaseModel",
     "IntIdPkMixin",
+    "ActiveMixin",
     "User",
 ]
