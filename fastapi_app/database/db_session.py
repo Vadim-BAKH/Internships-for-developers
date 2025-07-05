d3c5d1ae744cda6a82b4baf30f027be5efd95e17
PC <vadima64@mail.ru>
2025-07-05 15:57:23 +0300

    linter

--- fastapi_app/database/db_session.py
+++ fastapi_app/database/db_session.py
@@ -2,8 +2,11 @@
 
 from uuid import uuid4
 
-from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
-                                    create_async_engine)
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
 
 from fastapi_app.configs import settings
 
