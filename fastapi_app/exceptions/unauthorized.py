a1a0606f1cdb868f6a3e9b51fb1ae9f4be062ebe
PC <vadima64@mail.ru>
2025-07-06 11:05:38 +0300

    Exc type of token

--- fastapi_app/exceptions/unauthorized.py
+++ fastapi_app/exceptions/unauthorized.py
@@ -21,3 +21,23 @@ class InvalidToken(HTTPException):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid token error.",
         )
+
+
+class NotAccessTokenType(HTTPException):
+    """Модель исключения NotAccessTokenTipe"""
+
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token type. Expected access",
+        )
+
+
+class NotRefreshTokenType(HTTPException):
+    """Модель исключения NotRefreshTokenType"""
+
+    def __init__(self):
+        super().__init__(
+            status_code=401,
+            detail="Invalid token type. Expected refresh token.",
+        )
