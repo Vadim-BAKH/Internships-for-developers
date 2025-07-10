9bc6afcfb0234db6472b67dee66b9d6206ab37a4
PC <vadima64@mail.ru>
2025-07-10 17:38:31 +0300

    add init

--- fastapi_app/configs/__init__.py
+++ fastapi_app/configs/__init__.py
@@ -1,14 +1,13 @@
-"""
-Конфигурационные переменные проекта:
-- settings: глобальные настройки из .env
-- logger: сконфигурированный loguru
-- TEST_DB_URL: строка подключения к тестовой БД
-"""
+"""Инициализация переменных проекта."""
 
 from fastapi_app.configs.loguru_conf import logger
 from fastapi_app.configs.main_conf import settings
+from fastapi_app.configs.taskiq_broker import broker
+from fastapi_app.configs.validate_env import validate_settings
 
 __all__ = [
     "logger",
     "settings",
+    "broker",
+    "validate_settings",
 ]
